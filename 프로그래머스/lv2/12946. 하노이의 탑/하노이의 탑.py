6e762af59b689512bc55d3838e94064a5b553f60
codinglcy <107927270+codinglcy@users.noreply.github.com>
2023-04-06 17:07:26 +0900

    [level 2] Title: 하노이의 탑, Time: 9.90 ms, Memory: 18.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
@@ -0,0 +1,23 @@
+def solution(n):
+    answer = []
+    
+    def mini(num, start, go, result):
+        if num == 1:
+            result.append([start, go])
+        else:
+            mini(num-1, start, 6-start-go, result)
+            result.append([start, go])
+            mini(num-1, 6-start-go, go, result)
+        return result       
+    
+    return mini(n, 1, 3, answer)
+
+# 1: [[1,3]]
+# 2: [[1,2],[1,3],[2,3]]
+# 3: [[1,3],[1,2],[3,2],[1,3],[2,1],[2,3],[1,3]]
+#
+# n개를 3번에 옮기는 방법
+#   n-1개를 2번으로 옮긴다 
+#   3번에 가장 큰걸 옮긴다
+#   n-1개를 3번으로 옮긴다
+#        ...
